a19143caa294abf1d60386d2eb957f0d38db3808
Gaubbe <gabriel12125@gmail.com>
2024-04-29 17:53:50 -0400

    feat: added device creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
 use vulkano::VulkanLibrary;
 use vulkano::instance::{ Instance, InstanceCreateInfo };
 
@@ -13,4 +14,28 @@ fn main() {
         .expect("could not enumerate devices")
         .next()
         .expect("no devices available");
+
+    // Find the graphics queue family
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_i, queue_family_props)| {
+            queue_family_props.queue_flags.contains(QueueFlags::GRAPHICS)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+
+    // Create the logical device
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos : vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        })
+        .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
 }
