f664288613a14f426a5945d5d3e3e6d1c1bfe1ee
Gaubbe <gabriel12125@gmail.com>
2024-04-29 17:43:42 -0400

    feat: vulkan library setup and physical device finding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use vulkano::VulkanLibrary;
+use vulkano::instance::{ Instance, InstanceCreateInfo };
+
 fn main() {
-    println!("Hello, world!");
+    // Setup library and Vulkan instance
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default())
+        .expect("failed to create instance");
+
+    // Find physical device
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
 }
