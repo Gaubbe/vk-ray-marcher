005ccd62a2e6a88794542cc5e5223d15398ca2f7
Gaubbe <gabriel12125@gmail.com>
2024-08-28 15:03:14 -0400

    feat: added device creation

---
+++ src/vulkan/device.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+use vulkano::instance::Instance;
+use vulkano::device::{Device, DeviceCreateInfo, Queue, QueueCreateInfo, QueueFlags};
+use vulkano::device::physical::PhysicalDevice;
+
+pub fn create_device(instance: &Arc<Instance>) -> (Arc<Device>, Vec<Arc<Queue>>) {
+    let physical_device = choose_physical_device(instance);
+
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_i, props)| {
+            props.queue_flags.contains(QueueFlags::GRAPHICS)
+        })
+        .expect("Couldn't find a graphical queue family") as u32;
+
+    let (device, queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        })
+        .expect("Could not create Vulkan logical device.");
+
+    return (device, queues.collect());
+}
+
+fn choose_physical_device(instance: &Arc<Instance>) -> Arc<PhysicalDevice> {
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("Could not enumerate Vulkan physical devices.")
+        .next()
+        .expect("No Vulkan physical devices available");
+
+    return physical_device;
+}
--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -1,18 +1,26 @@
 use std::sync::Arc;
 use vulkano::instance::Instance;
+use vulkano::device::{Device, Queue};
 
 mod instance;
+mod device;
 
 pub struct VulkanContext {
     pub instance: Arc<Instance>,
+    pub device: Arc<Device>,
+    pub queues: Vec<Arc<Queue>>,
 }
 
 impl VulkanContext {
     pub fn new() -> VulkanContext {
         let instance = instance::create_vulkan_instance();
 
+        let (device, mut queues) = device::create_device(&instance);
+
         VulkanContext {
-            instance
+            instance,
+            device,
+            queues
         }
     }
 }
